46be235c1626f21efa4c0444188027cc10458735
AvantikaChauhan <70808331+AvantikaChauhan@users.noreply.github.com>
2021-02-10 19:06:57 +0530

    Update HomePage.py

--- POM/Pages/HomePage.py
+++ POM/Pages/HomePage.py
@@ -1,6 +1,6 @@
 class Home_page():
     def __init__(self, driver):
         self.driver = driver
-        self.signout_id = "nav-item-signout"
+        self.signout_id = " "
     def SignOut(self):
         self.driver.find_elements_by_id(self.signout_id).click()
