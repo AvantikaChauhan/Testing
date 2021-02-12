e7d4c1d4621bc5e085a46ab0ebbaa6b9efea2abe
AvantikaChauhan <70808331+AvantikaChauhan@users.noreply.github.com>
2021-02-12 09:04:15 +0530

    Update Login_page.py

--- POM/Pages/Login_page.py
+++ POM/Pages/Login_page.py
@@ -1,16 +1,12 @@
 class Login_page():
     def __init__(self, driver):
         self.driver = driver
-        self.account_id = ""
-        self.email_id = ""
-        self.continue_id = ""
+        self.user_id = ""
         self.password_id = ""
         self.submit_id   = ""
     def UserName(self, Uname):
-        self.driver.find_element_by_id(self.account_id).click()
-        self.driver.find_element_by_id(self.email_id).clear()
-        self.driver.find_element_by_id(self.email_id).send_keys(Uname)
-        self.driver.find_element_by_id(self.continue_id).click()
+        self.driver.find_element_by_id(self.user_id).clear()
+        self.driver.find_element_by_id(self.user_id).send_keys(Uname)
     def Password(self, password):
         self.driver.find_element_by_id(self.password_id).clear()
         self.driver.find_element_by_id(self.password_id).send_keys(password)
